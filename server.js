fdead52b8b2fedd69a03c18bf2752ec9928bce50
Ted Thayer <frederick.thayer@colorado.edu>
2018-11-21 02:21:57 -0700

    emptyied .env

--- server.js
+++ server.js
@@ -247,4 +247,4 @@ app.get('/', (req, res) => {
 });
 
 
-app.listen(process.env.PORT || 4000, () => console.log(`We are listening on port`));
+app.listen(process.env.PORT || 3000, () => { console.log("Express server listening on port"); });
