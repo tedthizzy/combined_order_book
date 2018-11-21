c94a3fda4b6fd73e4d403b3a8a53c3e90ade9bb0
Ted Thayer <frederick.thayer@colorado.edu>
2018-11-21 10:08:25 -0700

    tryagain

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
-// const fs = require('fs');
-// const pug = require('pug');
+const fs = require('fs');
+const pug = require('pug');
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
